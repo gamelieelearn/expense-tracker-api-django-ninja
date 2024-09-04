092c124b559ce36764cb9689a9176a8589837fa5
dat <snapeandcandy@gmail.com>
2024-09-04 23:09:47 +0700

    update model to use system's user

--- expense_tracker/models.py
+++ expense_tracker/models.py
@@ -1,13 +1,5 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-
-
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
 
 
 class ExpenseCategory(models.TextChoices):
@@ -24,7 +16,7 @@ class Expense(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100, choices=ExpenseCategory.choices)
     description = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     date = models.DateField()
 
     def __str__(self):
