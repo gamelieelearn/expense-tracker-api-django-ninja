68c2a6c320b1ffa43346d42d6f083f7971980ec3
dat <snapeandcandy@gmail.com>
2024-09-04 22:31:24 +0700

    init ninja

--- ninjaproject/urls.py
+++ ninjaproject/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+
+api = NinjaAPI()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls),
 ]
